a5ad3e2e6d844f81fc6204704a393180db8dcf1b
woodsj42 <woodsj42@users.noreply.github.com>
2015-06-10 13:27:31 -0400

    Delete 20150604222931_create_dnd35e_classes.rb

--- db/migrate/20150604222931_create_dnd35e_classes.rb
+++
@@ -1,9 +0,0 @@
-class CreateDnd35eClasses < ActiveRecord::Migration
-  def change
-    create_table :dnd35e_classes do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
