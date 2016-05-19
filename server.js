c646b6f71304ebec6915e88a3a9cbdad50e85982
Jake Woods <woodsj42@gmail.com>
2016-05-19 20:08:46 +0000

    added sword coast cantrips

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require("express");
-var http = require('http');
+var express  = require("express"),
+    http     = require('http'),
+    mongoose = require("mongoose");
+    
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
